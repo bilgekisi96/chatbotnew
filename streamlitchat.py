91b7194171110d8fddb7ce6beef650ff122e88e5
selim <selim.ozkok@yupanatech.com>
2024-12-22 15:57:46 +0300

    asc

--- streamlitchat.py
+++ streamlitchat.py
@@ -1,25 +1,4 @@
-from tornado.web import Application, RequestHandler
-from tornado.routing import Rule, PathMatches
-import gc
-import streamlit as st
+from tensorflow.keras.models import load_model
 
 
-@st.cache_resource()
-def setup_api_handler(uri, handler):
-    print("Setup Tornado. Should be called only once")
-
-    # Get instance of Tornado
-    tornado_app = next(o for o in gc.get_referrers(Application) if o.__class__ is Application)
-
-    # Setup custom handler
-    tornado_app.wildcard_router.rules.insert(0, Rule(PathMatches(uri), handler))
-    
-# === Usage ======
-class HelloHandler(RequestHandler):
-    def get(self):
-        with st.chat_message("assistant"):
-            st.markdown("Hello World")
-        self.write({'message': 'hello world'})
-
-# This setup will be run only once
-setup_api_handler('/api/hello', HelloHandler)
+model = load_model("moviesmodel.keras")
