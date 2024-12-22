371aca78da88d39ef0215ee60828defee630a612
selim <selim.ozkok@yupanatech.com>
2024-12-22 15:14:31 +0300

    dfb

--- streamlitchat.py
+++ streamlitchat.py
@@ -1,24 +1,35 @@
-from tornado.web import Application, RequestHandler
-from tornado.routing import Rule, PathMatches
-import gc
 import streamlit as st
+import multiprocessing
+
+must_reload_page = False
+
+def start_flask():
+    if not hasattr(st, 'already_started_server'):
+        st.already_started_server = True
+        must_reload_page = True
+
+        from flask import Flask
+
+        app = Flask(__name__)
+
+        @app.route('/foo')
+        def serve_foo():
+            return 'This page is served via Flask!'
+
+        app.run(port=8888)
+
+def reload_page():
+    if must_reload_page:
+        must_reload_page = False
+        st.experimental_rerun()
+
+if __name__=='__main__':
+    flask_process = multiprocessing.Process(target=start_flask)
+    reload_process = multiprocessing.Process(target=reload_page)
+    flask_process.start()
+    reload_process.start()
 
 
-@st.cache_resource()
-def setup_api_handler(uri, handler):
-    print("Setup Tornado. Should be called only once")
-
-    # Get instance of Tornado
-    tornado_app = next(o for o in gc.get_referrers(Application) if o.__class__ is Application)
-
-    # Setup custom handler
-    tornado_app.wildcard_router.rules.insert(0, Rule(PathMatches(uri), handler))
-    
-# === Usage ======
-class HelloHandler(RequestHandler):
-  def get(self):
-    self.write({'message': 'hello world'})
-    st.write("Hello from Tornado")
-
-# This setup will be run only once
-setup_api_handler('/api/hello', HelloHandler)
+# Your normal Streamlit app goes here:
+x = st.slider('Pick a number')
+st.write('You picked:', x)
