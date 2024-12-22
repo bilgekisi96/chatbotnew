2dfd096ae499fb51b8a194e22f660dd936194d60
selim <selim.ozkok@yupanatech.com>
2024-12-22 15:19:50 +0300

    afa

--- streamlitchat.py
+++ streamlitchat.py
@@ -16,7 +16,7 @@ def start_flask():
         def serve_foo():
             return 'This page is served via Flask!'
 
-        app.run(port=8501)
+        app.run(port=8001)
 
 def reload_page():
     if must_reload_page:
